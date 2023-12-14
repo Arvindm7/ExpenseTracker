167833a8e28944719898600de4f9eea9d38d7340
Arvindm7 <abinashmallick21@gmail.com>
2023-12-14 23:52:03 +0530

    1st modification

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Expense tracker
+        
         </a>
       </header>
     </div>
