e67244b224c89a3514339b429efdea984fedc53f
imdinnesh <121331053+imdinnesh@users.noreply.github.com>
2023-12-17 14:32:11 +0530

    Completed AddIcome Section

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom/client'
 import { GlobalStyle } from './styles/GlobalStyle';
+import { GlobalProvider } from './context/globalContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle/>
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>
 );
 
