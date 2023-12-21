2240268849b5ea6c0f32ae3372cb331c7ebf8a06
imdinnesh <121331053+imdinnesh@users.noreply.github.com>
2023-12-21 13:25:51 +0530

    Added Expense section

--- frontend/src/Components/Expenses/ExpenseForm.js
+++ frontend/src/Components/Expenses/ExpenseForm.js
@@ -1,16 +1,14 @@
-import React, { useState } from "react"
-import styled from "styled-components"
-import DatePicker from "react-datepicker"
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
-import { useGlobalContext } from "../../context/globalContext"
-import Button from "../Button/Button"
-import { plus } from "../../utils/icons";
+import { useGlobalContext } from '../../context/globalContext';
+import Button from '../Button/Button';
+import { plus } from '../../utils/icons'
 
 
-function Form(){
-
-    const {addIncome,getIncomes}=useGlobalContext()
-
+function ExpenseForm() {
+    const {addExpense,error} = useGlobalContext()
     const [inputState, setInputState] = useState({
         title: '',
         amount: '',
@@ -25,10 +23,9 @@ function Form(){
         setInputState({...inputState, [name]: e.target.value})
     }
 
-    const handleSubmit=e=>{
+    const handleSubmit = e => {
         e.preventDefault()
-        addIncome(inputState)
-        //getIncomes()
+        addExpense(inputState)
         setInputState({
             title: '',
             amount: '',
@@ -38,15 +35,15 @@ function Form(){
         })
     }
 
-
     return (
-        <FormStyled onSubmit={handleSubmit}>
+        <ExpenseFormStyled onSubmit={handleSubmit}>
+            {error && <p className='error'>{error}</p>}
             <div className="input-control">
                 <input 
                     type="text" 
                     value={title}
                     name={'title'} 
-                    placeholder="Salary Title"
+                    placeholder="Expense Title"
                     onChange={handleInput('title')}
                 />
             </div>
@@ -54,7 +51,7 @@ function Form(){
                 <input value={amount}  
                     type="text" 
                     name={'amount'} 
-                    placeholder={'Salary Amount'}
+                    placeholder={'Expense Amount'}
                     onChange={handleInput('amount')} 
                 />
             </div>
@@ -71,14 +68,14 @@ function Form(){
             </div>
             <div className="selects input-control">
                 <select required value={category} name="category" id="category" onChange={handleInput('category')}>
-                    <option value=""  disabled >Select Option</option>
-                    <option value="salary">Salary</option>
-                    <option value="freelancing">Freelancing</option>
-                    <option value="investments">Investments</option>
-                    <option value="stocks">Stocks</option>
-                    <option value="bitcoin">Bitcoin</option>
-                    <option value="bank">Bank Transfer</option>  
-                    <option value="youtube">Youtube</option>  
+                    <option value="" disabled >Select Option</option>
+                    <option value="education">Education</option>
+                    <option value="groceries">Groceries</option>
+                    <option value="health">Health</option>
+                    <option value="subscriptions">Subscriptions</option>
+                    <option value="takeaways">Takeaways</option>
+                    <option value="clothing">Clothing</option>  
+                    <option value="travelling">Travelling</option>  
                     <option value="other">Other</option>  
                 </select>
             </div>
@@ -87,7 +84,7 @@ function Form(){
             </div>
             <div className="submit-btn">
                 <Button 
-                    name={'Add Income'}
+                    name={'Add Expense'}
                     icon={plus}
                     bPad={'.8rem 1.6rem'}
                     bRad={'30px'}
@@ -95,30 +92,28 @@ function Form(){
                     color={'#fff'}
                 />
             </div>
-
-
-        </FormStyled>
-
+        </ExpenseFormStyled>
     )
 }
 
-const FormStyled=styled.form`
+
+const ExpenseFormStyled = styled.form`
     display: flex;
     flex-direction: column;
     gap: 2rem;
     input, textarea, select{
-    font-family: inherit;
-    font-size: inherit;
-    outline: none;
-    border: none;
-    padding: .5rem 1rem;
-    border-radius: 5px;
-    border: 2px solid #fff;
-    background: transparent;
-    resize: none;
-    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
-    color: rgba(34, 34, 96, 0.9);
-    &::placeholder{
+        font-family: inherit;
+        font-size: inherit;
+        outline: none;
+        border: none;
+        padding: .5rem 1rem;
+        border-radius: 5px;
+        border: 2px solid #fff;
+        background: transparent;
+        resize: none;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        color: rgba(34, 34, 96, 0.9);
+        &::placeholder{
             color: rgba(34, 34, 96, 0.4);
         }
     }
@@ -127,6 +122,7 @@ const FormStyled=styled.form`
             width: 100%;
         }
     }
+
     .selects{
         display: flex;
         justify-content: flex-end;
@@ -146,11 +142,5 @@ const FormStyled=styled.form`
             }
         }
     }
-
-
 `;
-
-
-
-
-export default Form
+export default ExpenseForm
